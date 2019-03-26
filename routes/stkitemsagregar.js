7ef9048002ea5f68101a0122737264072bb12c7e
Rogelio Martin Corsino <rcorsino@gmail.com>
2019-03-26 10:23:31 -0300

    stkitemsagregar

--- routes/stkitemsagregar.js
+++ routes/stkitemsagregar.js
@@ -3,6 +3,9 @@ var router = express.Router();
 var path = require('path');
 var moment = require('moment');
 var conexion = require('./conexion');
+// var stkiemsinsertar = require('./stkitemsinsertar');
+
+var nroitem = 0;
 
 
 moment.locale('es');
@@ -17,27 +20,45 @@ conexion.connect(function(err) {
 });
 
 
+//SELECT max(idStkItems) FROM BasesGenerales.StkItems  where StkItemsRubro = 10 and StkItemsGrupo = 1;
+//select hecho en mysql para buscar el item más grande y agregar 1
 
-router.post('/', function(req, res) {
+router.get('/', async function(req, res) {
+    var StkItemsGrupo = req.query.id2;
+    var StkItemsRubro = req.query.id3;
 
-  var registro = {
-  //  idStkGrupo : req.body.idStkGrupo,
-   
-    idStkRubro : req.body.idStkRubro,
-    StkRubroCodGrp : req.body.StkRubroCodGrp,
-    StkRubroDesc : req.body.StkRubroDesc,
-    StkRubroAbr : req.body.StkRubroAbr,
-    StkRubroProv : req.body.StkRubroProv,
-    StkRubroAncho : req.body.StkRubroAncho,
-    StkRubroPres : req.body.StkRubroPres,
-    StkRubroUM : req.body.StkRubroUM,
-    StkRubroCosto : req.body.StkRubroCosto,
-    StkRubroTM : req.body.StkRubroTM
-   
-  }
-  console.log(registro);
 
-        conexion.query('INSERT INTO StkRubro SET ?', registro, 
+
+    // StkItemsDesc : req.body.StkItemsDesc,
+    // StkItemsCantidad : req.body.StkItemsCantidad,
+    // StkItemsFAct : req.body.StkItemsFAct,
+    // StkItemsMin : req.body.StkItemsMin,
+    // StkItemsMax: req.body.StkItemsMax,
+ 
+  conexion.query('Select max(idStkItems) as UltItem from StkItems where StkItemsGrupo  = ' + StkItemsGrupo  + ' and  StkItemsRubro  = ' + StkItemsRubro, 
+  function(err, result) {
+    if (err) {
+        console.log(err);
+    } else {
+        res.json(result);
+      console.log(result[0].UltItem);
+      console.log([result]);
+      nroitem = result[0].UltItem + 1;
+      console.log(nroitem);
+      var registro = {
+        idStkItems : nroitem,
+        StkItemsGrupo : StkItemsGrupo,
+        StkItemsRubro : StkItemsRubro,
+        StkItemsDesc : req.body.StkItemsDesc,
+        StkItemsCantidad : req.body.StkItemsCantidad,
+        StkItemsFAct : req.body.StkItemsFAct,
+        StkItemsMin : req.body.StkItemsMin,
+        StkItemsMax: req.body.StkItemsMax
+      
+      }
+    }
+    
+    conexion.query('INSERT INTO StkItems SET ?', registro, 
         function(err, result) {
             if (err) {
                 console.log('ERROR ');
@@ -46,10 +67,23 @@ router.post('/', function(req, res) {
                 res.json(result.rows);
             
             }
+        //    return
         });
+    })
 });
 
 
 
+// stkiemsinsertar.insertar(nroitem)
 
-module.exports = router;
+
+   
+  // StkItemsGrupo : req.body.StkItemsDesc,
+    // StkItemsRubro : req.body.StkItemsRubro,
+    // StkItemsDesc : req.body.StkItemsDesc,
+    // StkItemsCantidad : req.body.StkItemsCantidad,
+    // StkItemsFAct : req.body.StkItemsFAct,
+    // StkItemsMin : req.body.StkItemsMin,
+    // StkItemsMax: req.body.StkItemsMax,
+
+module.exports =router;
